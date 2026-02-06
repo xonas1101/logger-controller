3fc9df67fdfe017b46aede7abb6131c342a32c17
xonas1101 <aarushsingh1305@gmail.com>
2026-02-06 08:08:34 +0000

    finishing up test
    Signed-off-by: xonas1101 <aarushsingh1305@gmail.com>

--- internal/controller/logger_controller_test.go
+++ internal/controller/logger_controller_test.go
@@ -21,47 +21,108 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	loggerv1 "github.com/xonas1101/logger-controller/api/v1"
 )
 
+func testPod(name, namespace string) *v1.Pod {
+	return &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels: map[string]string{
+				"test": "logger",
+			},
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:  "pause",
+					Image: "registry.k8s.io/pause:3.9",
+				},
+			},
+		},
+	}
+}
+
+func testDeployment(name, namespace string) *appsv1.Deployment {
+	replicas := int32(1)
+
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels: map[string]string{
+				"test": "logger",
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": name,
+				},
+			},
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": name,
+						"test": "logger",
+					},
+				},
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name:  "pause",
+							Image: "registry.k8s.io/pause:3.9",
+						},
+					},
+				},
+			},
+		},
+	}
+}
 var _ = Describe("Logger Controller", func() {
 	Context("When reconciling a logger", func() {
 		const loggerName = "test-logger"
 
-		const (
-			watchedns = "logger-watched"
-			unwatchedns = "logger-unwatched"
-			crName = "logger-sample"
-		)
-
 		ctx := context.Background()
 
-		logger := &loggerv1.Logger{}
 		typeNamespacedName := types.NamespacedName{
 			Name:      loggerName,
 			Namespace: "default", // TODO(user):Modify as needed
 		}
+    watchedns := &v1.Namespace{
+      ObjectMeta: metav1.ObjectMeta{
+      Name: "watchedns",
+    	},
+  	}
+
+		unwatchedns := &v1.Namespace{
+      ObjectMeta: metav1.ObjectMeta{
+      Name: "unwatchedns",
+    	},
+  	}
 
 		BeforeEach(func() {
 			By("creating namespaces used by the Logger scope")
-
-			for _, ns := range []string{watchedns, unwatchedns} {
-				err := k8sClient.Create(ctx, &v1.Namespace{
-					ObjectMeta: metav1.ObjectMeta{
-						Name: ns,
-					},
-				})
-
-				if err != nil && !errors.IsAlreadyExists(err) {
-					Expect(err).NotTo(HaveOccurred())
-				}
+			err := k8sClient.Get(ctx, types.NamespacedName{Name: watchedns.Name}, &v1.Namespace{})
+      if err != nil {
+				Expect(errors.IsNotFound(err)).To(BeTrue())
+				err = k8sClient.Create(ctx, watchedns)
+				Expect(err).NotTo(HaveOccurred())
+			}
+			err = k8sClient.Get(ctx, types.NamespacedName{Name: unwatchedns.Name}, &v1.Namespace{})
+      if err != nil {
+				Expect(errors.IsNotFound(err)).To(BeTrue())
+				err = k8sClient.Create(ctx, unwatchedns)
+				Expect(err).NotTo(HaveOccurred())
 			}
 		})
 
@@ -75,7 +136,21 @@ var _ = Describe("Logger Controller", func() {
 			By("Cleanup the specific logger instance Logger")
 			Expect(k8sClient.Delete(ctx, logger)).To(Succeed())
 		})
-		It("should successfully reconcile the logger", func() {
+		It("watches only the specified namespace when one namespace is provided", func() {
+				logger := &loggerv1.Logger{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      loggerName,
+						Namespace: watchedns.Name,
+					},
+					Spec: loggerv1.LoggerSpec{
+						Scope: loggerv1.ScopeSpec{
+							Type:       "Namespace",
+							Namespace: watchedns.Name,
+						},
+					},
+				}
+
+	Expect(k8sClient.Create(ctx, logger)).To(Succeed())
 			By("Reconciling the created logger")
 			controllerReconciler := &LoggerReconciler{
 				Client: k8sClient,
